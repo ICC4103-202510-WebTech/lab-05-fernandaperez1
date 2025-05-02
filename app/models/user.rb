860e55a070c66f12bc480a56e8f15eadd19b834e
Fernanda Pérez <faperez1@miuandes.cl>
2025-05-02 19:16:17 -0400

    lab7 listo

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,8 @@
 class User < ApplicationRecord
-    validates :email, presence: true, uniqueness: true
-    validates :first_name, presence: true
-    validates :last_name, presence: true
-    has_many :messages, dependent: :destroy
-    has_many :sent_chats, class_name: 'Chat', foreign_key: 'sender_id', dependent: :destroy
-    has_many :received_chats, class_name: 'Chat', foreign_key: 'receiver_id', dependent: :destroy
-  end
+  validates_presence_of :email, :first_name, :last_name
+  validates :email, uniqueness: true
+
+  has_many :messages, dependent: :destroy
+  has_many :sent_chats,     class_name: 'Chat', foreign_key: :sender_id,   dependent: :destroy
+  has_many :received_chats, class_name: 'Chat', foreign_key: :receiver_id, dependent: :destroy
+end
