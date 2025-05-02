52ac51a3b8f936a3628eb20b63fa3f701a04dcc0
Fernanda Pérez <faperez1@miuandes.cl>
2025-05-02 19:14:33 -0400

    home page agregada

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  get "home/index"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
@@ -14,4 +15,6 @@ Rails.application.routes.draw do
   resources :users
   resources :chats
   resources :messages
+  root 'home#index'
+
 end
